70cc24e4749ebdb37f5708fd5400200c81c0b80a
dorofeev <v.dorofeev@tetra-soft.ru>
2024-01-29 12:18:16 +0300

    fix

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Create and send fanout message by RabbitMQ
+
+The essence of this project is to demonstrate sending messages to all applications by RabbitMQ
+
+## Test and run in docker - two instance
+
+```
+./dock.sh
+```
--- src/main/java/com/prod/rabbit/service/Runner.java
+++ src/main/java/com/prod/rabbit/service/Runner.java
@@ -4,14 +4,16 @@ import com.prod.rabbit.dto.PriceExchange;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Slf4j
+@Lazy(false)
 @Component
-public class Runner implements CommandLineRunner {
+public class Runner {
 
     @Value("${app.name}")
     private String appName;
@@ -19,8 +21,8 @@ public class Runner implements CommandLineRunner {
     @Autowired
     private ProducerService producerService;
 
-    @Override
-    public void run(String... args) throws Exception {
+    @Scheduled(fixedDelay = 10000, initialDelay = 4000)
+    public void run() {
         PriceExchange price = new PriceExchange(appName, "BTC", "USDT", new BigDecimal("1110.2"));
         log.info(" !!! Being send message: {}", price);
         producerService.broadcastMessage(price);
